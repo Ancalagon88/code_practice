4e3e25f29a7193a9cc0336daf0f9d358af346df7
Daniel Phillips <danielphillips@.workfront.com>
2020-11-13 10:29:25 -0700

    advent of code 2019-1

---
+++ aoc_2019_01.py
@@ -0,0 +1,43 @@
+# Prompt: https://adventofcode.com/2019/day/1
+
+sum_of_fuel = 0
+mass_divider = 3
+mass_subtractor = 2
+attempts = 0
+try_limit = 5
+fuel_per_module = []
+
+
+def fuel_calculator(mass):
+    fuel = mass/mass_divider
+    if mass % mass_divider:
+        fuel = int(fuel)
+    fuel = fuel - mass_subtractor
+    return fuel
+
+
+def get_number():
+    for retries in range(try_limit):
+        try:
+            whole_number = int(input(
+                "Please enter a whole number: "))
+        except:
+            print("Incorrect input, please enter a whole number using number keys.\n")
+            continue
+        if isinstance(whole_number, int):
+            return whole_number
+    else:
+        print("\nYou done messed up A-a-ron! (ノಠ益ಠ)ノ彡┻━┻\n")
+        exit()
+
+
+print("How many modules will your space ship have?")
+number_of_modules = get_number()
+
+for module in range(number_of_modules):
+    print("What is the weight of module number {}\n".format(module + 1))
+    module_weight = get_number()
+    module_fuel_req = fuel_calculator(module_weight)
+    fuel_per_module.append(module_fuel_req)
+
+print("The total required fuel for this trip is {}".format(int(sum(fuel_per_module))))
